a550972505ee702194087c27c506ffc9f367ae7e
ahmedrakha10 <arakha70@gmail.com>
2018-09-17 09:38:46 +0200

    ahed

---
+++ librarian/notapprove.php
@@ -0,0 +1,9 @@
+<?php
+include 'connect.php';
+ $id = $_GET['id'];
+ mysqli_query($con,"UPDATE student_registeration SET status ='no' where id=$id");
+
+?>
+<script type ="text/javascript">
+        window.location = "display_student_info.php";
+    </script>
